a8db3e5eeba4b7b7b0d1157ccba4bd75b2841c3e
timusss0 <mustikavia86@gmail.com>
2023-07-14 13:01:48 +0700

    ASIKK SUDAH BISA

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const notifier = require('node-notifier')
-const moment = require('moment')
-const agrTime = process.argv.slice[2]
+const notifier = require('node-notifier');
+const moment = require('moment');
+const agrTime = process.argv.slice(2)
 
 const POMODORO_DURATION = agrTime[0]
 const BREAK_DURATION = agrTime[1]
 
 let isWorking = false
-let pomodoroTime = 0
+let remainingTime = 0
 
 
 function formattingTime(totalSecond){
     const duration = moment.duration(totalSecond , 'second')
     const hours = duration.hours().toString().padStart(2,'0')
     const minutes = duration.minutes().toString().padStart(2, '0')
-    const second = duration.minutes().toString().padStart(2, '0')
+    const second = duration.seconds().toString().padStart(2, '0')
 
 
     return `${hours}:${minutes}:${second}`
@@ -25,18 +25,18 @@ function startTime(duration){
         isWorking = !isWorking
         remainingTime = duration * 60
 
-        const time = setInterval(() => {
+        const timer = setInterval(() => {
             remainingTime--
 
-            const formattingTime = formattingTime(remainingTime)
+            const formattedTime = formattingTime(remainingTime)
 
 
-            console.log(`${isWorking ? 'work' : 'break'}: ${formattingTime}`);
+            console.log(`${isWorking ? 'work' : 'break'}: ${formattedTime}`);
 
             if(remainingTime === 0){
                 clearInterval(timer)
-                mutifier.notifier({
-                    title: isWorking ? 'working time' : 'break time!',
+                notifier.notify({
+                    title: isWorking ? 'Break time!!' : 'Good work!',
                     Message:  isWorking ? 'Good work' : 'Good break!',
                     sound: true,
                     wait: true
